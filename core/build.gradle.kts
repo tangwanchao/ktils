03b33a3a6f1c28483f79a95e3e50155468b0c1fa
tangwanchao <18284228392@163.com>
2025-09-24 16:29:12 +0800

    build: source 依赖版本升级为 3.0.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -49,7 +49,7 @@ afterEvaluate {
                 // 配置发布的元数据
                 groupId = "me.twc"
                 artifactId = "ktils"
-                version = "0.0.3"
+                version = "0.0.4"
             }
         }
     }
