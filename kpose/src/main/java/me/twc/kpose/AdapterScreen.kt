6b6d6ee341da5f6a84daa94678ae549a0f2e7f22
tangwanchao <18284228392@163.com>
2025-10-10 16:58:55 +0800

    feature: 1.AdapterScreen.kt 使用 LocalResources.current

--- kpose/src/main/java/me/twc/kpose/AdapterScreen.kt
+++ kpose/src/main/java/me/twc/kpose/AdapterScreen.kt
@@ -2,9 +2,9 @@ package me.twc.kpose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Density
+import androidx.compose.ui.platform.LocalResources
 
 /**
  * @author 唐万超
@@ -17,7 +17,7 @@ fun AdaptScreen(
     content: @Composable () -> Unit
 ) {
     val fontScale = LocalDensity.current.fontScale
-    val displayMetrics = LocalContext.current.resources.displayMetrics
+    val displayMetrics = LocalResources.current.displayMetrics
     val widthPixels = displayMetrics.widthPixels.toFloat()
     CompositionLocalProvider(
         LocalDensity provides Density(
@@ -31,8 +31,7 @@ fun AdaptScreen(
 
 @Composable
 fun SystemScreen(content: @Composable () -> Unit) {
-    val context = LocalContext.current
-    val displayMetrics = context.resources.displayMetrics
+    val displayMetrics = LocalResources.current.displayMetrics
     val systemDensity = displayMetrics.density
     val fontScale = LocalDensity.current.fontScale
 
